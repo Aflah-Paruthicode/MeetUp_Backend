717023a9e6b656ad9de50703743e0fccbfb98cc0
Muhammed Aflah TK <aflutech2@gmail.com>
2025-11-11 16:02:03 +0530

    Add User Api, Get All Users APi

--- src/app.js
+++ src/app.js
@@ -20,8 +20,32 @@ app.post("/signup", async (req, res) => {
 
 });
 
+app.get('/users', async (req,res) => { 
+  const name = req.body.name;
+  try {
+    const users = await User.find({name : name});
+    if(users.length !== 0) { 
+      res.send(users)
+    } else { 
+      res.status(404).send('User Not Found')
+    }
+  } catch (err) { 
+    res.status(404).send('something went wrong '+err.message)
+  }
+})
+
+app.get('/feed', async (req,res) => { 
+
+  try { 
+    const users = await User.find({});
+    res.send(users)
+  } catch (error) { 
+    res.status(404).send('something went wrong')
+  }
+})
+
 connectDb()
-  .then(() => {
+  .then(() => { 
     console.log("Database connection is did");
     app.listen(8080, () => {
       console.log("Server Started");
