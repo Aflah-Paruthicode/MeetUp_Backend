f641af26951f984d552621dd0072597eaae69888
Muhammed Aflah TK <aflutech2@gmail.com>
2025-11-13 20:04:00 +0530

    DB validation

--- src/app.js
+++ src/app.js
@@ -1,73 +1,75 @@
-
 const express = require("express");
 const { connectDb } = require("./config/database");
 const User = require("./model/user");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/signup", async (req, res) => { 
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
 
-  console.log(req.body)
-  const user = new User(req.body);
-
-  try { 
-    await user.save();
-    res.send('Document added successfull')
+  try {
+    const { email } = req.body;
+    const isEmailUnique = await User.find({ email: email });
+    if (isEmailUnique.length !== 0) {
+      res.status(404).send("Email Already Exists");
+    } else {
+      const user = new User(req.body);
+      await user.save();
+      res.send("Document added successfull");
+    }
   } catch (error) {
-    res.status(400).send('Error Occured In Adding Doc', error.message);
+    res.status(400).send("Error Occured In Adding Doc - "+ error.message);
   }
-
 });
 
-app.get('/users', async (req,res) => { 
+app.get("/users", async (req, res) => {
   const name = req.body.name;
   try {
-    const users = await User.find({name : name});
-    if(users.length !== 0) { 
-      res.send(users)
-    } else { 
-      res.status(404).send('User Not Found')
+    const users = await User.find({ name: name });
+    if (users.length !== 0) {
+      res.send(users);
+    } else {
+      res.status(404).send("User Not Found");
     }
-  } catch (err) { 
-    res.status(404).send('something went wrong '+err.message)
+  } catch (err) {
+    res.status(404).send("something went wrong " + err.message);
   }
-})
+});
 
-app.get('/feed', async (req,res) => { 
-
-  try { 
+app.get("/feed", async (req, res) => {
+  try {
     const users = await User.find({});
-    res.send(users)
-  } catch (error) { 
-    res.status(404).send('something went wrong')
+    res.send(users);
+  } catch (error) {
+    res.status(404).send("something went wrong");
   }
-})
+});
 
-app.delete('/user',async (req,res) => { 
-  const id = req.body.id
-  try { 
-    const user = await User.findByIdAndDelete({_id:id}) 
-    res.status(200).send(user)
-  } catch (err) { 
-    res.status(404).send('Something Went Wrong')
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+  try {
+    const user = await User.findByIdAndDelete({ _id: id });
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(404).send("Something Went Wrong");
   }
-})
+});
 
-app.patch('/user',async (req,res) => { 
+app.patch("/user", async (req, res) => {
   const userId = req.body.id;
   const data = req.body;
 
-  try { 
-    const user = await User.findByIdAndUpdate({_id : userId}, data);
-    res.status(200).send('User Updated Successfull', user)
-  } catch (error) { 
-    res.status(404).send('Something Went Wrong');
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data,{runValidators:true});
+    res.status(200).send("User Updated Successfull", user);
+  } catch (error) {
+    res.status(404).send("Something Went Wrong");
   }
-})
+});
 
 connectDb()
-  .then(() => { 
+  .then(() => {
     console.log("Database connection is did");
     app.listen(8080, () => {
       console.log("Server Started");
--- src/model/user.js
+++ src/model/user.js
@@ -5,17 +5,38 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: {
     type: String,
-    required : true
+    required : true,
+    minLength : 4,
+  },
+  email : {
+    type : String,
+    lowercase : true,
+    required : true,
+    trim : true
   },
   favMovie: {
     type: String,
+  },
+  gender : {
+    type : String,
+    required : true,
+    validate(value) {
+      if(!['male','female','others'].includes(value)) {
+        throw new Error('Gender Data is invalid')
+      }
+    }
   },
   place: {
     type: String,
   },
   studying: {
     type: String,
+    default : 'programming'
   },
-});
+},
+{
+    timestamps : true,
+  },
+);
 
 module.exports = mongoose.model("user", userSchema);
