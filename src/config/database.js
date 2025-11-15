8dff33f1a5247aca70e8ea4c7f0ad940724fcff0
Muhammed Aflah TK <aflutech2@gmail.com>
2025-11-15 23:54:39 +0530

    DB issue solved

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const pass = process.env.DB_PASSWORD;
 
 const connectDb = async () => {
-  await mongoose.connect(
-    "mongodb+srv://aflah:@chatapp.ziqd6ut.mongodb.net/"
-  );  
+  await mongoose.connect("mongodb+srv://aflah:"+pass+"@chattime.myb23jr.mongodb.net/");
 };
 
-module.exports = {connectDb}
+module.exports = { connectDb };
