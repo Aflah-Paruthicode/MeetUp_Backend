031a27d83e030ff6a6ed54421adc2b9740fea072
Muhammed Aflah TK <aflutech2@gmail.com>
2025-11-30 23:41:42 +0530

    Schema have to re build

--- src/model/user.js
+++ src/model/user.js
@@ -1,66 +1,75 @@
-
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const validator = require('validator')
+const validator = require("validator");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required : true,
-    minLength : 4,
-    maxLength : 30,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 30,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      trim: true,
+      maxLength: 50,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password Not Valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default : 'https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3485.jpg?w=360',
+      required: true,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("PhotoUrl not valid");
+        }
+      },
+    },
+    gender: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender Data is invalid");
+        }
+      },
+    },
+    about: {
+      type: String,
+      maxLength: 150,
+      minLength: 4,
+    },
+    skills: {
+      type: [String],
+      maxLength: 30,
+      minLength: 4,
+    },
   },
-  email : {
-    type : String,
-    lowercase : true,
-    required : true,
-    trim : true,
-    maxLength : 50,
-    validate(value) {
-      if(!validator.isEmail(value)) {
-        throw new Error('Email is not valid')
-      }
-    }
-
-  },
-  password : {
-    type : String,
-    required : true,
-    validate(value) {
-      if(!validator.isStrongPassword(value)) {
-        throw new Error('Password Not Valid')
-      }
-    }
-  },
-  favMovie: {
-    type: String,
-    maxLength : 30,
-    minLength : 1,
-  },
-  gender : {
-    type : String,
-    required : true,
-    validate(value) {
-      if(!['male','female','others'].includes(value)) {
-        throw new Error('Gender Data is invalid')
-      }
-    }
-  },
-  place: {
-    type: String,
-    maxLength : 30,
-    minLength : 4
-  },
-  studying: {
-    type: String,
-    default : 'programming',
-    maxLength : 30,
-    minLength : 4
-  },
-},
-{
-    timestamps : true,
-  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("user", userSchema);
